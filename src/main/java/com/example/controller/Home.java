93ab99a9451241cf29c69622c82d1da6d2f2b177
lilou <lyloou@qq.com>
2018-09-12 20:09:34 +0800

    add: log4j

--- src/main/java/com/example/controller/Home.java
+++ src/main/java/com/example/controller/Home.java
@@ -1,12 +1,18 @@
 package com.example.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
+
+    private static final Logger logger = Logger.getLogger(Home.class);
+
+
     @RequestMapping("/")
     public String home() {
+        logger.debug("=========== logger info =========== ");
         return "index";
     }
 }
